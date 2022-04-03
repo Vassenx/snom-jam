646d01a86a6ef19c9dd0f6887b8f0a262a9d3b8b
Samantha Difeo <sdifeo1@gmail.com>
2022-04-03 13:04:28 -0400

    Bubbles attack boss with Unity's updated OnParticleTrigger()

---
+++ SnomJamGame/Assets/ParticleTrigger.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTrigger : MonoBehaviour
+{
+    [SerializeField] private Attack boss;
+    [SerializeField] private float damagePerParticle = 5f;
+    private List<ParticleSystem.Particle> enteredParticles;
+    private ParticleSystem ps;
+    private void Start()
+    {
+        enteredParticles = new List<ParticleSystem.Particle>();
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    private void OnParticleTrigger()  
+    {
+        enteredParticles.Clear();
+        //Get all particles that entered a box collider
+        int numEntered = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enteredParticles, out var enterData);
+        
+        for (int i = 0; i < numEntered; i++)
+        {
+            if (enterData.GetCollider(i, 0).gameObject == boss.gameObject)
+            {
+                boss.DamageDealt(damagePerParticle);
+            }
+        }
+    }
+}
